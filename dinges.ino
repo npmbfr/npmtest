f4f879f87d866e65ef9ec686deae87b67f23ee21
bart <bfriso@gmail.com>
2022-12-14 22:11:05 +0100

    wimpel toegevoegd

--- dinges.ino
+++ dinges.ino
@@ -1,4 +1,5 @@
 /** dit is een eerste opzetje voor ik wert niet wat
  * en bestaat voornamelijk uit commentaar
  * */
-const boolean USE_FLAG = 1;
+const boolean USE_FLAG   = 1;
+const boolean USE_WIMPLE = 0;
